57f9a1c2a194ec3b99d4a8c23e9befbd2dafb626
Diego Dias <diego2007jf@gmail.com>
2024-10-05 23:18:29 -0300

    added error handling

--- app/controllers/gifs_controller.py
+++ app/controllers/gifs_controller.py
@@ -3,6 +3,7 @@ import time
 from datetime import datetime
 
 from sqlalchemy import func
+from sqlalchemy.exc import IntegrityError
 
 from app.models.gifs_model import GifsModel
 from app.services.database_service import DatabaseService
@@ -15,16 +16,23 @@ class GifsController:
 
     async def add_gif(self, gif: str, user: Optional[discord.User], weight: int = 5):
         async with self.database_service.session() as session:
-            await session.add(GifsModel(
-                url=gif,
-                weight=weight,
-                gif_name=f'{datetime.now()}_tenor-gif_{time.time()}.gif',
-                created_at=datetime.now(),
-                added_by_user_id=user.id if user is not None else None
-            ))
-            await session.commit()
+            try:
+                await session.add(GifsModel(
+                    url=gif,
+                    weight=weight,
+                    gif_name=f'{datetime.now()}_tenor-gif_{time.time()}.gif',
+                    created_at=datetime.now(),
+                    added_by_user_id=user.id if user is not None else None
+                ))
+                await session.commit()
+            except IntegrityError:
+                raise AlreadyExistsError('this gif already exists.')
 
     async def get_random_gif(self) -> GifsModel:
         async with self.database_service.session() as session:
             gif = await session.query(GifsModel).order_by(func.random()).first()
             return gif
+
+
+class AlreadyExistsError(Exception):
+    pass
