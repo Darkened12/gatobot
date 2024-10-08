fe5b56fcef4a7bcb6ef6a5aa0c3ddcc28b306c99
Diego Dias <diego2007jf@gmail.com>
2024-10-08 02:37:45 -0300

    updated commands_cog.py

--- app/bot/cogs/commands_cog.py
+++ app/bot/cogs/commands_cog.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
 
+from app.controllers.gifs_controller import GifsController, AlreadyExistsError
+
 
 class CommandsCog(discord.Cog):
-    def __int__(self, bot: discord.Bot):
+    def __int__(self, bot: discord.Bot, gifs_controller: GifsController):
         self.bot = bot
+        self.controller = gifs_controller
 
     @commands.group(name='gif')
     async def gif_group(self, ctx: discord.ApplicationContext):
@@ -12,5 +15,11 @@ class CommandsCog(discord.Cog):
 
     @gif_group.command(name='add', description='adds a gif to the gato database')
     async def add_gif(self, ctx: discord.ApplicationContext, url: str):
-
-        await ctx.send(f"Gif {url} added!")
+        try:
+            await self.controller.add_gif(
+                gif=url,
+                user=ctx.author
+            )
+            return await ctx.respond(f"Gif {url} added!", ephemeral=True)
+        except AlreadyExistsError:
+            return await ctx.respond(f"Error! Either the gif already exists or something else happened!")
