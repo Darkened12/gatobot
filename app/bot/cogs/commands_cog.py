70c36fe5524841cd0bd90630bcabceab514b5ded
Diego Dias <diego2007jf@gmail.com>
2024-10-08 02:51:04 -0300

    added logging

--- app/bot/cogs/commands_cog.py
+++ app/bot/cogs/commands_cog.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 
 from app.controllers.gifs_controller import GifsController, AlreadyExistsError
+from app.services.logging_service import logger
 
 
 class CommandsCog(discord.Cog):
@@ -11,7 +12,8 @@ class CommandsCog(discord.Cog):
 
     @commands.group(name='gif')
     async def gif_group(self, ctx: discord.ApplicationContext):
-        return await ctx.respond('Available subcommads: add', ephemeral=True)
+        await ctx.respond('Available subcommads: add', ephemeral=True)
+        return logger.info(f'user "{ctx.author}" used command "gif".')
 
     @gif_group.command(name='add', description='adds a gif to the gato database')
     async def add_gif(self, ctx: discord.ApplicationContext, url: str):
@@ -20,6 +22,10 @@ class CommandsCog(discord.Cog):
                 gif=url,
                 user=ctx.author
             )
-            return await ctx.respond(f"Gif {url} added!", ephemeral=True)
+            await ctx.respond(f"Gif {url} added!", ephemeral=True)
+            return logger.info(f'user "{ctx.author}" added the gif "{url}".')
         except AlreadyExistsError:
-            return await ctx.respond(f"Error! Either the gif already exists or something else happened!")
+            await ctx.respond(f"Error! Either the gif already exists or something else happened!")
+            return logger.error(f'error {AlreadyExistsError} ocurred while executing command "add" - user: {ctx.author}'
+                                f' gif: {url}')
+
