f49183a84b3f05522161c1bf889024524deab57e
Diego Dias <diego2007jf@gmail.com>
2024-10-08 03:02:33 -0300

    updated bot

--- app/bot/__init__.py
+++ app/bot/__init__.py
@@ -1,59 +1,21 @@
 import os
-
 import discord
-import asyncio
-import random
 
-from datetime import datetime
-from dotenv import load_dotenv
-from typing import List
+from dotenv import load_dotenv as __load_dotenv
 
-load_dotenv()
+from app.services.database_service import DatabaseService
+from app.controllers.gifs_controller import GifsController
+from app.bot.cogs.listeners_cog import ListenersCog
+from app.bot.cogs.commands_cog import CommandsCog
 
-intents = discord.Intents.default()
-intents.messages = True
+__load_dotenv()
+__intents = discord.Intents.default()
+__intents.messages = True
 
-bot = discord.Bot(intents=intents)
+bot = discord.Bot(intents=__intents)
+__database = DatabaseService(os.environ.get('DATABASE_SECRET'))
+__controller = GifsController(__database)
 
-CHANNELS: List[int] = [974759972036542484]
+bot.add_cog(ListenersCog(bot, __controller))
+bot.add_cog(CommandsCog(bot, __controller))
 
-
-async def send_random_cat():
-    for channel_id in CHANNELS:
-        channel = bot.get_channel(channel_id)
-        selected_item = random.choices(
-            EMOJIS,
-            weights=[40, 40, 20, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-            k=1
-        )[0]
-        await channel.send(selected_item)
-
-
-async def sleep():
-    await asyncio.sleep(1800)
-
-
-@bot.event
-async def on_ready():
-    print(f'Logged at {datetime.now()} as {bot.user}')
-
-    while True:
-        await send_random_cat()
-        await sleep()
-
-
-@bot.event
-async def on_message(message: discord.Message):
-    if message.author == bot.user:
-        return
-
-    if bot.user in message.mentions:
-        return await message.reply(random.choice(EMOJIS))
-
-    if message.reference:
-        original_message = await message.channel.fetch_message(message.reference.message_id)
-        if bot.user in original_message.mentions:
-            return await message.reply(random.choice(EMOJIS))
-
-
-bot.run(os.environ.get('GATO_BOT'))
