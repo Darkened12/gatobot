08746970c9388570d63ace699138596171b8bfb5
Diego Dias <diego2007jf@gmail.com>
2024-10-08 02:18:19 -0300

    refactoring

--- app/bot/gifs-cog/listeners_cog.py
+++ app/bot/gifs-cog/listeners_cog.py
@@ -11,7 +11,7 @@ from app.controllers.gifs_controller import GifsController
 from app.services import randomizer_cat_service, parallel_task_runner_service
 
 
-class GifsCog(commands.Cog):
+class ListenersCog(commands.Cog):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
         self.gifs_controller = GifsController(database_service=DatabaseService(os.environ.get('DATABASE_SECRET')))
@@ -24,7 +24,7 @@ class GifsCog(commands.Cog):
         logger.info('Database initiated successfully!')
 
         for channel_model in CHANNELS:
-            parallel_task_runner_service.run_parallel_task(self._cat_sender(channel_model))
+            parallel_task_runner_service.run_parallel_task(self.__cat_sender(channel_model))
 
         logger.info(f'Cog "{self.__cog_name__}" ready at "{datetime.now()}".')
 
@@ -32,7 +32,7 @@ class GifsCog(commands.Cog):
     async def on_message(self, message: discord.Message):
         pass
 
-    async def _cat_sender(self, channel_model: ChannelsModel):
+    async def __cat_sender(self, channel_model: ChannelsModel):
         channel = self.bot.get_channel(channel_model.id)
         time_in_seconds = channel_model.sleep_time_in_seconds
         while True:
