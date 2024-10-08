4335dcb9c4ea48ac4fe39e497407b5d2183d7c6c
Diego Dias <diego2007jf@gmail.com>
2024-10-08 19:48:16 -0300

    DatabaseService revamp

--- app/services/database_service.py
+++ app/services/database_service.py
@@ -1,7 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-
 # necessary to import everything so the Base registers.
 from app.models.gifs_model import GifsModel
 from app.models.base_model import Base
@@ -10,10 +9,10 @@ from app.models.base_model import Base
 class DatabaseService:
     Base = Base
 
-    def __init__(self, dsn: str, dsn_connector: str, new_database: bool = False):
+    def __init__(self, dsn: str, dsn_connector: str, create_database: bool = False):
         self.dsn_connector = dsn_connector
         self.dsn = self.dsn_connector + dsn
-        self.new_database = new_database
+        self.create_database = create_database
         self.engine = None
         self.session = None
 
@@ -25,9 +24,14 @@ class DatabaseService:
         self.session = await self._get_session()
         return self
 
+    async def update_schema(self):
+        if self.engine is not None:
+            async with self.engine.begin() as conn:
+                return await conn.run_sync(self.Base.metadata.create_all)
+
     async def _get_engine(self):
         engine = create_async_engine(self.dsn, pool_recycle=1200)
-        if self.new_database:
+        if self.create_database:
             async with engine.begin() as conn:
                 await conn.run_sync(self.Base.metadata.drop_all)
                 await conn.run_sync(self.Base.metadata.create_all)
@@ -35,6 +39,6 @@ class DatabaseService:
 
     async def _get_session(self):
         async_session = sessionmaker(
-            self.engine, expire_on_commit=False, class_=AsyncSession
+            bind=self.engine, expire_on_commit=False, class_=AsyncSession
         )
         return async_session
