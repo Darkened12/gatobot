18b44948ad20b30e71c48c2a3072eed160639968
Diego Dias <diego2007jf@gmail.com>
2024-10-11 23:28:03 -0300

    added logging

--- app/bot/cogs/react_to_messages_cog.py
+++ app/bot/cogs/react_to_messages_cog.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from typing import Union
 from app.bot.channels import CHANNELS
+from app.services.logging_service import logger
 
 keywords = {
     "meow": "😺",
@@ -34,14 +35,23 @@ class ReactToMessagesCog(discord.Cog):
         self.allowed_channels = [channel.id for channel in CHANNELS]
 
     @commands.Cog.listener()
+    async def on_ready(self):
+        logger.info(f'cog "{self.__cog_name__}" ready.')
+
+    @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if message.author != self.bot.user and message.channel.id in self.allowed_channels:
             for word in message.content.split():
                 if word.lower() in keywords.keys():
-                    return await message.add_reaction(keywords[word.lower()])
+                    emoji = keywords[word.lower()]
+                    await message.add_reaction(emoji)
+                    return logger.info(f'triggered keyword reaction "{emoji}" on "{message.channel.name}" channel'
+                                       f' on message "{message.content}".')
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction: discord.Reaction, user: Union[discord.User, discord.Member]):
         if reaction.message.channel.id in self.allowed_channels:
             if reaction.emoji.id in [1280387632492449946, 1180027630871904276]:
-                return await reaction.message.add_reaction(reaction.emoji)
+                await reaction.message.add_reaction(reaction.emoji)
+                return logger.info(f'added reaction "{reaction.emoji}" on "{reaction.message.channel.name}" channel'
+                                   f' on message "{reaction.message.content}".')
