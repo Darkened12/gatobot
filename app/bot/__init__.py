29141c91ae9dc40e5b3d721b58e1518ec7bd0c3a
Diego Dias <diego2007jf@gmail.com>
2024-10-11 23:21:25 -0300

    added ReactToMessagesCog

--- app/bot/__init__.py
+++ app/bot/__init__.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv as __load_dotenv
 from app.services.database_service import DatabaseService
 from app.controllers.gifs_controller import GifsController
 from app.bot.cogs.listeners_cog import ListenersCog
+from app.bot.cogs.react_to_messages_cog import ReactToMessagesCog
 from app.bot.cogs.commands_cog import CommandsCog
 
 
@@ -17,5 +18,5 @@ __database = DatabaseService(dsn_connector='sqlite+aiosqlite:///', dsn=os.enviro
 __controller = GifsController(__database)
 
 bot.add_cog(ListenersCog(bot, __controller))
+bot.add_cog(ReactToMessagesCog(bot))
 bot.add_cog(CommandsCog(bot, __controller))
-
