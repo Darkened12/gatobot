30cd0243c30247decaa735aeffcd96189647b7e9
Diego Dias <diego2007jf@gmail.com>
2024-10-11 23:28:53 -0300

    updated timer

--- app/bot/channels.py
+++ app/bot/channels.py
@@ -3,8 +3,8 @@ from typing import List
 
 
 _CHANNELS_TO_WORK_ON: List[List[int]] = [
-    [974759972036542484, 1800],  # hoyo
-    [974759995843428422, 7200]  # blue archive
+    [974759972036542484, 1800],  # hoyo, 30min
+    [974759995843428422, 14400]  # blue archive, 4h
 ]
 
 
