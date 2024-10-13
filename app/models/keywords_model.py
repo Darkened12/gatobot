923b36968b98019fe4108d3f3bf554280fae2cdd
Diego Dias <diego2007jf@gmail.com>
2024-10-12 22:21:05 -0300

    updated keywords model

--- app/models/keywords_model.py
+++ app/models/keywords_model.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, VARCHAR
+import sqlalchemy
+from sqlalchemy import Column, Integer, VARCHAR, DateTime, BigInteger
 from sqlalchemy.orm import relationship
+from datetime import datetime
 
 from .base_model import Base
 from .links_keywords_association_model import LinksKeywordsAssociation
@@ -13,4 +15,8 @@ class KeywordsModel(Base):
 
     id = Column(Integer, primary_key=True)
     keyword = Column(VARCHAR(20), nullable=False, unique=True, index=True)
+    created_at = Column(DateTime, default=datetime.now(), server_default=sqlalchemy.func.current_timestamp(),
+                        nullable=False)
+    created_by_user_id = Column(BigInteger, default=243332147379830785,
+                                server_default=sqlalchemy.text('243332147379830785'), nullable=False)
     links = relationship('LinksModel', secondary=LinksKeywordsAssociation, back_populates='links')
