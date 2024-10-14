b0bc26b5d4f4777c6ac664cbd19b9666442b4ab1
Diego Dias <diego2007jf@gmail.com>
2024-10-14 19:42:43 -0300

    updated bot

--- app/bot/__init__.py
+++ app/bot/__init__.py
@@ -3,7 +3,11 @@ import discord
 from dotenv import load_dotenv as __load_dotenv
 
 from app.services.database_service import DatabaseService
+
 from app.controllers.links_controller import LinksController
+from app.controllers.keywords_controller import KeywordsController
+from app.controllers.emojis_controller import EmojisController
+
 from app.bot.cogs.listeners_cog import ListenersCog
 from app.bot.cogs.react_to_messages_cog import ReactToMessagesCog
 from app.bot.cogs.commands_cog import CommandsCog
@@ -19,6 +23,11 @@ bot = discord.Bot(intents=__intents, owner_id=int(os.environ.get('OWNER_ID')))
 __database = DatabaseService(dsn_connector='sqlite+aiosqlite:///', dsn=os.environ.get('DATABASE'))
 __controller = LinksController(__database)
 
-bot.add_cog(ListenersCog(bot, __controller))
-bot.add_cog(ReactToMessagesCog(bot))
-bot.add_cog(CommandsCog(bot, __controller))
+# bot.add_cog(ListenersCog(bot, __controller))
+# bot.add_cog(CommandsCog(bot, __controller))
+bot.add_cog(ReactToMessagesCog(
+    bot=bot,
+    keywords_controller=KeywordsController(__database),
+    emoji_controller=EmojisController(__database),
+    links_controller=LinksController(__database),
+))
