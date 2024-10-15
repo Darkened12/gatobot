b1abc42a7d65cfdd41c655b59a78945f1ba4b6b8
Diego Dias <diego2007jf@gmail.com>
2024-10-15 12:31:33 -0300

    increased hoyo to 1h

--- app/models/channels_dataset.py
+++ app/models/channels_dataset.py
@@ -3,7 +3,7 @@ from typing import List
 
 
 _CHANNELS_TO_WORK_ON: List[List[int]] = [
-    [974759972036542484, 1800],  # hoyo, 30min
+    [974759972036542484, 3600],  # hoyo, 60min
     [974759995843428422, 14400]  # blue archive, 4h
 ]
 
