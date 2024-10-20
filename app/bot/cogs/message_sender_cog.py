987bead40cb2c7a3399053a370319fe874d41d21
Diego Dias <diego2007jf@gmail.com>
2024-10-20 01:59:08 -0300

    increased cooldown

--- app/bot/cogs/message_sender_cog.py
+++ app/bot/cogs/message_sender_cog.py
@@ -15,7 +15,7 @@ from app.services.logging_service import logger
 class MessageSenderCog(discord.Cog):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
-        self.cooldown_message_service = CooldownService(120)
+        self.cooldown_message_service = CooldownService(240)
         self.cat = CatHappiness()
         self.emotes = {'happy': '<:gato:1180027630871904276>',
                        'sad': '<:gatodespair:1280387632492449946>'}
